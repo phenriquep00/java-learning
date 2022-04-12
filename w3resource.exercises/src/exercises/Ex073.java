711cda8e7c5d092d9d66b3c4d692a02d323e8624
Pedro Lima <contact.pedrolima@gmail.com>
2022-04-12 16:18:07 -0300

    Resolved Ex074

--- w3resource.exercises/src/exercises/Ex073.java
+++ w3resource.exercises/src/exercises/Ex073.java
@@ -3,10 +3,21 @@ package exercises;
 import java.util.Scanner;
 
 public class Ex073 {
-    //73. Write a Java program to create a new string taking first and last characters from two given strings. If the length of either string is 0 use "#" for missing character. 
-    public static void main(String[] args){
+    //72. Write a Java program to create a new string taking first three characters from a given string. If the length of the given string is less than 3 use "#" as substitute characters.
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        scan.close();
+        System.out.print("input first string: ");
+        String str1 = scan.nextLine();
+        System.out.print("input second string: ");
+        String str2 = scan.nextLine();
+
+        if (str1.equals(" ") || str1.length() == 0) {
+            System.out.print("#" + str2.charAt(str2.length() - 1));
+        } else if (str2.equals(" ") || str2.length() == 0) {
+            System.out.print(str1.substring(0, 1) + '#');
+        } else {
+            System.out.print(str1.substring(0, 1) + str2.charAt(str2.length() - 1));
+        }
     }
 }
--- w3resource.exercises/src/exercises/Ex074.java
+++ w3resource.exercises/src/exercises/Ex074.java
@@ -1,12 +1,16 @@
 package exercises;
 
-import java.util.Scanner;
+
+import java.lang.reflect.Array;
 
 public class Ex074 {
     // 74. Write a Java program to test if 10 appears as either the first or last element of an array of integers. The length of the array must be greater than or equal to 2.
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
+        int[] arr = {10, -20, 0, 30, 40, 60, 10};
 
-        scan.close();
+        boolean answer = (arr[0] == 10 || arr[-1] == 10) ? true : false;
+
+        System.out.print(answer);
+
     }
 }
