1b5628ee6ea4710b45a2516b4c79257d0975c827
Pedro Lima <contact.pedrolima@gmail.com>
2022-04-12 15:18:38 -0300

    Resolved Ex071

--- w3resource.exercises/src/exercises/Ex071.java
+++ w3resource.exercises/src/exercises/Ex071.java
@@ -1,12 +1,16 @@
 package exercises;
 
-import java.util.Scanner;
 
 public class Ex071 {
     //71. Write a Java program to create the concatenation of the two strings except removing the first character of each string. The length of the strings must be 1 and above.
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // todo
-        scan.close();
+        String str1 = "Python";
+        String str2 = "Tutorial";
+
+        System.out.println(str1.substring(1) + str2.substring(1));
+        /*
+        * The String class method .substring() can be used to get
+        * the substring from an original string
+        * */
     }
 }
