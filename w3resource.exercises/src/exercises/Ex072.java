ee32e7fef24ad7a303659c1cea4b8fc7aed67c4c
Pedro Lima <contact.pedrolima@gmail.com>
2022-04-12 16:04:07 -0300

    Resolved Ex072

--- w3resource.exercises/src/exercises/Ex072.java
+++ w3resource.exercises/src/exercises/Ex072.java
@@ -7,6 +7,17 @@ public class Ex072 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        scan.close();
+        System.out.print("input first string: ");
+        String str1 = scan.nextLine();
+        System.out.print("input second string: ");
+        String str2 = scan.nextLine();
+
+        if (str1.equals(" ") || str1.length() == 0) {
+            System.out.print("#" + str2.charAt(str2.length() - 1));
+        } else if (str2.equals(" ") || str2.length() == 0) {
+            System.out.print(str1.substring(0, 1) + '#');
+        } else {
+            System.out.print(str1.substring(0, 1) + str2.charAt(str2.length() - 1));
+        }
     }
 }
