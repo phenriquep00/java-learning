9e82350269eec269677378ea672702a0e652d49e
Pedro Lima <contact.pedrolima@gmail.com>
2022-04-12 16:21:41 -0300

    Resolved Ex075

--- w3resource.exercises/src/exercises/Ex075.java
+++ w3resource.exercises/src/exercises/Ex075.java
@@ -1,12 +1,10 @@
 package exercises;
 
-import java.util.Scanner;
 
 public class Ex075 {
     //75. Write a Java program to test if the first and the last element of an array of integers are same. The length of the array must be greater than or equal to 2.
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        scan.close();
+        int[] arr = {50, -20, 0, 30, 40, 60, 10};
+        System.out.print((arr[0] == arr[arr.length - 1]));
     }    
 }
